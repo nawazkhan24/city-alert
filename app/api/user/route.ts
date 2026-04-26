031ffe857fe4acafb66a7cd901da9e6d189e5fcb
nawazkhan24 <nk298270@gmail.com>
2026-04-26 16:45:02 +0530

    type error corrected

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -6,14 +6,16 @@ import { authOptions } from "../auth/[...nextauth]/route";
 export async function GET() {
   await connectDB();
 
-  const session = await getServerSession(authOptions);
-  if (!session) {
-    return Response.json({ error: "Not logged in" });
+  const session: any = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    return Response.json({ error: "Not logged in" }, { status: 401 });
   }
 
   const user = await User.findById(session.user.id);
 
   return Response.json({
+    name: user.name,
     email: user.email,
     city: user.city,
     keyword: user.keyword,
